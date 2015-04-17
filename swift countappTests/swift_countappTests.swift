7ccb9315156ca1fe79e59bef996b9b97fc2858f6
Naoto <naoto@Naoto-no-MacBook-Pro.local>
2015-04-17 19:46:48 +0900

    Initial Commit

---
+++ swift countappTests/swift_countappTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_countappTests.swift
+//  swift countappTests
+//
+//  Created by Naoto on 2015/04/17.
+//  Copyright (c) 2015年 Naoto. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swift_countappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
